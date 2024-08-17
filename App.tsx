a8c63f79810dba75134aecc35a16bbede71bf77c
Jamiu Olaroungbe <olaabiodun157@gmail.com>
2024-08-17 08:57:21 +0100

    Initial Petme commit

--- App.tsx
+++ App.tsx
@@ -3,18 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-3xl text-red-600'>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
