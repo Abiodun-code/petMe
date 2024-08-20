c564fa032da1cc4407917e8efb1832f5f724f808
Jamiu Olaroungbe <olaabiodun157@gmail.com>
2024-08-20 21:25:29 +0100

    Add google font to expo

--- App.tsx
+++ App.tsx
@@ -1,11 +1,33 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { Keyboard, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Keyboard, Pressable } from 'react-native';
 import AuthStack from './src/navigation/AuthStack';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
+import { Inter_100Thin, Inter_400Regular, Inter_500Medium, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
+import { useFonts } from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    Inter_100Thin,
+    Inter_900Black,
+    Inter_700Bold,
+    Inter_500Medium,
+    Inter_400Regular
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return (
     <Pressable onPress={Keyboard.dismiss} className='flex-1'>
       <SafeAreaProvider>
--- src/screens/auth/Onboarding.tsx
+++ src/screens/auth/Onboarding.tsx
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
 const Onboarding = () => {
   return (
-    <View>
-      <Text>Onboarding</Text>
+    <View className='flex-1 bg-white'>
+      <View className='flex justify-center items-center'>
+        <Image source={require("../../../assets/appImage/onboard.png")}
+          style={{ width: wp(100), height: hp(30) }}
+          className='bg-center object-center'
+        />
+        <View  className='flex items-center px-3'>
+          <Text className='uppercase' style={{ fontSize: hp(3), fontFamily:"Inter_900Black", marginBottom: 20 }}>PetMe</Text>
+          <Text style={{ fontSize: 16, fontFamily:'Inter_400Regular' }}>Discover new friends, meet new pets, and build a strong bond.</Text>
+        </View>
+      </View>
+      <View className='flex-1 justify-end items-center'>
+        <Text style={{ fontSize: 14, color: '#2196F3', marginTop: 10 }}>Already have an account? Login</Text>
+      </View>
     </View>
   )
 }
