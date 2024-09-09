865f3cd3451b91693ee6826e725a32307e347947
Jamiu Olaroungbe <olaabiodun157@gmail.com>
2024-09-09 10:00:34 +0100

    Setting page design

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -12,7 +12,7 @@ const Onboarding = () => {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.navigate("/login")
+      router.replace("/(drawer)/(tabs)/home")
     }, 500);
 
     return () => clearTimeout(timer);
--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,77 +1,11 @@
 import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import {
-  GoogleSignin,
-  GoogleSigninButton,
-  isErrorWithCode,
-  isSuccessResponse,
-  statusCodes
-} from '@react-native-google-signin/google-signin'
-import { EXPO_WEB_CLIENT_ID, EXPO_IOS_CLIENT_ID } from "@env"
 
 const Login = () => {
-
-  useEffect(() => {
-    GoogleSignin.configure({
-      webClientId: EXPO_WEB_CLIENT_ID,
-      iosClientId: EXPO_IOS_CLIENT_ID,
-      offlineAccess: true,
-      forceCodeForRefreshToken: true,
-      profileImageSize: 120,
-      scopes: ['profile', 'email'],
-    });
-  }, []);
-
-  const [state, setState] = useState('')
-
-  const signIn = async () => {
-    try {
-      await GoogleSignin.hasPlayServices();
-      const response = await GoogleSignin.signIn();
-      if (isSuccessResponse(response)) {
-        const userInfo = response.data
-        setState( userInfo);
-        
-      } else {
-        // sign in was cancelled by user
-      }
-    } catch (error) {
-      if (isErrorWithCode(error)) {
-        switch (error.code) {
-          case statusCodes.IN_PROGRESS:
-            // operation (eg. sign in) already in progress
-            break;
-          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
-            // Android only, play services not available or outdated
-            break;
-          default:
-          // some other error happened
-        }
-      } else {
-        // an error that's not related to google sign in occurred
-      }
-    }
-    console.log(state.user)
-  };
-
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Welcome</Text>
-      {state ? (
-        // If userInfo is not null, display user information
-        <View style={{ alignItems: 'center' }}>
-          <Text>User is signed in!</Text>
-          <Text>Email: {state.user.email}</Text>
-        </View>
-      ) : (
-        // If userInfo is null, display the Google Sign-In button
-        <GoogleSigninButton
-          size={GoogleSigninButton.Size.Wide}
-          color={GoogleSigninButton.Color.Dark}
-          onPress={signIn}
-        />
-      )}
     </View>
   )
 }
